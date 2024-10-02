cd593b136b8ae50d2e203f8a965b1a1abff31dcf
James Chen <jam.chen@mac.com>
2024-10-02 19:04:06 +0800

    youTubeVideoUrl to store

--- src/stores/project.ts
+++ src/stores/project.ts
@@ -4,11 +4,12 @@ export const useProjectStore = defineStore('project', {
   state: () => ({
     videoFilePath: undefined as string | undefined,
     audioFilePath: undefined as string | undefined,
+    youTubeVideoUrl: undefined as string | undefined,
     videoCurrentTime: 0 as number,
     editingSubtitle: false as boolean,
   }),
 
   persist: {
-    pick: ['videoFilePath', 'audioFilePath'],
+    pick: ['videoFilePath', 'audioFilePath', 'youTubeVideoUrl'],
   },
 });
